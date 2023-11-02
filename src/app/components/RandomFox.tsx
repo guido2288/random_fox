1f936524c8f0d8a9ed1bb57abada56b7f2aed6c1
guido2288 <guido2288@gmail.com>
2023-11-01 21:45:37 -0300

    Se agrega lazy load

--- src/app/components/RandomFox.tsx
+++ src/app/components/RandomFox.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useRef } from "react";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 type Props = {image:string};
 
 const RandomFox = ( {image}:Props ) :JSX.Element => {
 
+  const node = useRef<HTMLImageElement>(null);
+
+  
+
+
   return (
-    <img className="w-80 rounded-md" src={image}/>
+    <LazyLoadImage ref={node} className="w-80 rounded-md" src={image} effect="blur"/>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from "react";
+import type { MouseEventHandler } from "react";
 import RandomFox from "./components/RandomFox";
 
 export default function Home() {
@@ -12,21 +13,26 @@ export default function Home() {
     return Math.random().toString(36).substr(2,9);
   };
 
-
   type ImageItems = { id:string, url:string };
 
-  const [images, setImages] = useState<Array<ImageItems>>([
-    { id:generateId(), url:`https://randomfox.ca/images/${getRandomNumber()}.jpg`},
-    { id:generateId(), url:`https://randomfox.ca/images/${getRandomNumber()}.jpg`},
-    { id:generateId(), url:`https://randomfox.ca/images/${getRandomNumber()}.jpg`},
-    { id:generateId(), url:`https://randomfox.ca/images/${getRandomNumber()}.jpg`},
+  const [images, setImages] = useState<Array<ImageItems>>([]);
 
-  ]);
+  const addNewFox: MouseEventHandler<HTMLButtonElement>  = (event) => {
+    event.preventDefault();
+    const newImageItem: ImageItems = { 
+      id:generateId(), 
+      url:`https://randomfox.ca/images/${getRandomNumber()}.jpg`
+    };
+
+    setImages([...images, newImageItem])
+  }
 
 
   return (
-    <main className="flex flex-col justify-center items-center">
-      <h1>Hello fox!</h1>
+    <main className="flex flex-col justify-center items-center font-nunito">
+      <h1 className=" text-4xl my-4 font-bold">Hello fox!</h1>
+
+      <button className="px-4 py-2 bg-slate-700 text-white rounded-md hover:bg-slate-600" onClick={addNewFox}>Add new 🦊</button>
       {
         images.map(({id, url}) => (
           <div key={id} className="p-4">
